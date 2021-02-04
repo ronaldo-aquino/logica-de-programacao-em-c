4afffae7c424ff93fbc4767b3ddbda970c8a10f8
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-04 20:54:12 -0300

    Exercícios de notas escolares

--- logica-de-progrramacao-em-c/letNotas/main.c
+++ logica-de-progrramacao-em-c/letNotas/main.c
@@ -3,6 +3,23 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char nome[80];
+    float nota1, nota2, nota3, media;
+
+    printf("Digite o nome do aluno: ");
+    gets(nome);
+    __fpurge(stdin);
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+
+    media = (nota1+nota2+nota3) / 3;
+
+    printf("O aluno %s tem media: %.2f \n", nome, media);
+
+
     return 0;
 }
