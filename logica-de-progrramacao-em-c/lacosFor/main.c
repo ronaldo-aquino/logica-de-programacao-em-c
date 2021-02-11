76af57f46332b42cdf950c31bb96bae45477f2d6
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-10 22:05:41 -0300

    Laço de repetição for

---
+++ logica-de-progrramacao-em-c/lacosFor/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero, resultado;
+
+    for(int i = 0; i <= 5; i++) {
+        printf("Numero %d \n", i);
+    }
+
+    /*printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    resultado = numero * 3;
+    printf("O resultado foi: %d \n", resultado);*/
+
+    return 0;
+}
